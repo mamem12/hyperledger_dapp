1b6643a4778f27fa989beb3982f871f2b6eaab3c
mamem12 <mamem.dev@gmail.com>
2022-08-18 12:01:59 +0900

    init, totalsupply, balanceof 메소드 구현

--- chaincode.go
+++ chaincode.go
@@ -28,6 +28,44 @@ type Metadata struct {
 func (cc *Chaincode) Init(stub shim.ChaincodeStubInterface) sc.Response {
 	_, params := stub.GetFunctionAndParameters()
 	fmt.Println("Init called with params: ", params)
+
+	return shim.Success(nil)
+}
+
+// Invoke is called as a result of an application request to run the chaincode.
+func (cc *Chaincode) Invoke(stub shim.ChaincodeStubInterface) sc.Response {
+	fcn, params := stub.GetFunctionAndParameters()
+	fmt.Println("Invoke()", fcn, params)
+
+	switch fcn {
+	case "init":
+		return cc.init(stub, params)
+	case "totalSupply":
+		return cc.totalSupply(stub, params)
+	case "balanceOf":
+		return cc.balanceOf(stub, params)
+	case "transfer":
+		return cc.transfer(stub, params)
+	case "allowance":
+		return cc.allowance(stub, params)
+	case "approve":
+		return cc.approve(stub, params)
+	case "transferFrom":
+		return cc.transferFrom(stub, params)
+	case "increaseAllowance":
+		return cc.increaseAllowance(stub, params)
+	case "decreaseAllowance":
+		return cc.decreaseAllowance(stub, params)
+	case "mint":
+		return cc.mint(stub, params)
+	case "burn":
+		return cc.burn(stub, params)
+	default:
+		return sc.Response{Status: 404, Message: "404 Not Found", Payload: nil}
+	}
+}
+
+func (cc *Chaincode) init(stub shim.ChaincodeStubInterface, params []string) sc.Response {
 	if len(params) != 4 {
 		return shim.Error("incorrect number of parameter")
 	}
@@ -67,68 +105,65 @@ func (cc *Chaincode) Init(stub shim.ChaincodeStubInterface) sc.Response {
 	return shim.Success(nil)
 }
 
-// Invoke is called as a result of an application request to run the chaincode.
-func (cc *Chaincode) Invoke(stub shim.ChaincodeStubInterface) sc.Response {
-	fcn, params := stub.GetFunctionAndParameters()
-	fmt.Println("Invoke()", fcn, params)
-
-	switch fcn {
-	case "totalSupply":
-		return cc.totalSupply(stub, params)
-	case "balanceOf":
-		return cc.balanceOf(stub, params)
-	case "transfer":
-		return cc.transfer(stub, params)
-	case "allowance":
-		return cc.allowance(stub, params)
-	case "approve":
-		return cc.approve(stub, params)
-	case "transferFrom":
-		return cc.transferFrom(stub, params)
-	case "increaseAllowance":
-		return cc.increaseAllowance(stub, params)
-	case "decreaseAllowance":
-		return cc.decreaseAllowance(stub, params)
-	case "mint":
-		return cc.mint(stub, params)
-	case "burn":
-		return cc.burn(stub, params)
-	default:
-		return sc.Response{Status: 404, Message: "404 Not Found", Payload: nil}
-	}
-}
-
+// totalSupply is query function
+// params is tokenName
+// Returns the amount of token in existence
 func (cc *Chaincode) totalSupply(stub shim.ChaincodeStubInterface, params []string) sc.Response {
+
 	// a number of params must be one
 	if len(params) != 1 {
-		return shim.Error("incorrect number of parameter")
+		return shim.Error("totalSupply only 1 params")
 	}
 
 	tokenName := params[0]
 
-	// Get ERC20 Metadata
-	metadata := Metadata{}
+	// get metadata
+	metadata := &Metadata{}
 	metadataBytes, err := stub.GetState(tokenName)
 	if err != nil {
-		return shim.Error("failed to GetState, error: " + err.Error())
-	}
-	err = json.Unmarshal(metadataBytes, &metadata)
-	if err != nil {
-		return shim.Error("failed to Unmarshal, error: " + err.Error())
+		errMsg := fmt.Sprintf("failed to getstate from totalSupply, error : %s", err.Error())
+		return shim.Error(errMsg)
 	}
 
-	// Convert TotalSupply to Bytes
-	totalSupplyBytes, err := json.Marshal(metadata.TotalSupply)
+	err = json.Unmarshal(metadataBytes, metadata)
 	if err != nil {
-		return shim.Error("failed to Marshal totalSupply, error: " + err.Error())
+		errMsg := fmt.Sprintf("failed to unmarshal from totalSupply, error : %s", err.Error())
+		return shim.Error(errMsg)
 	}
-	fmt.Println(tokenName + "'s totalSupply is " + string(totalSupplyBytes))
 
-	return shim.Success(totalSupplyBytes)
+	// convert metadata to bytes
+	totalsupplyBytes, err := json.Marshal(metadata.TotalSupply)
+	if err != nil {
+		errMsg := fmt.Sprintf("failed to marshal from totalSupply, error : %s", err.Error())
+		return shim.Error(errMsg)
+	}
+
+	fmt.Println(tokenName + "'s totalsupply is " + string(totalsupplyBytes))
+
+	return shim.Success(totalsupplyBytes)
 }
 
+// totalSupply is query function
+// params is address
+// Returns the amount of tokens owned by address
 func (cc *Chaincode) balanceOf(stub shim.ChaincodeStubInterface, params []string) sc.Response {
-	return shim.Success([]byte("balanceOf call !!"))
+	// a number of params must be one
+	if len(params) != 1 {
+		return shim.Error("balanceOf only 1 params")
+	}
+
+	address := params[0]
+
+	// get balance
+	amountBytes, err := stub.GetState(address)
+	if err != nil {
+		errMsg := fmt.Sprintf("failed to getstate from balanceOf, error : %s", err.Error())
+		return shim.Error(errMsg)
+	}
+
+	fmt.Println(address + "'s totalsupply is " + string(amountBytes))
+
+	return shim.Success(amountBytes)
 }
 
 func (cc *Chaincode) transfer(stub shim.ChaincodeStubInterface, params []string) sc.Response {
